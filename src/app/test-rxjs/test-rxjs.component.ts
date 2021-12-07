e0d357de69bc33b19751ac9d0e2374b45e9d415c
Salma Rakha <salmarakha75@gmail.com>
2021-12-07 16:40:46 +0200

    use map operator

--- src/app/test-rxjs/test-rxjs.component.ts
+++ src/app/test-rxjs/test-rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { fromEvent, Observable, Subscriber, from, timer, interval, of, finalize, Subscription, connectable } from 'rxjs';
+import { fromEvent, Observable, Subscriber, from, timer, interval, of, finalize, Subscription, connectable, map } from 'rxjs';
 
 @Component({
   selector: 'app-test-rxjs',
@@ -8,87 +8,93 @@ import { fromEvent, Observable, Subscriber, from, timer, interval, of, finalize,
 })
 export class TestRxjsComponent implements OnInit {
 
-  // create method is deprecated
-  observable = new Observable((observer: Subscriber<any>) => {
-    observer.next("Hello");
-    observer.next("World");
-  });
-
-  // create an observable from a DOM event
-  domEventObservable = fromEvent(document, 'click');
-
-  // create an observable from a promise
-  private promise = new Promise((res, rej) => {
-    setTimeout(() => {
-      res("Resolved!");
-    }, 4000);
-  });
-  // fromPromise method is now wrapped inside the from method
-  observablePromise = from(this.promise);
-
-  // create an observable timer
-  observableTimer = timer(5000);
-
-  // create observable time interval (emits something every interval)
-  observableInterval = interval(1000);
-
-  // obsrvable of static values
-  staticValueObservable = of("anything", ["you", "want"], 23, true, {cool: "stuff"});
-
-  /* Cold vs Hot observables */
-  // Cold observable example
-  cold = new Observable(observer => {
-    observer.next(Math.random());
-  });
-
-  // Hot observable example
-  private randomNum = Math.random();
-  hot = new Observable(observer => {
-    observer.next(this.randomNum);
-  })
-
-
   constructor() { }
 
   ngOnInit(): void {
-    this.observable.subscribe(value => console.log(value));
+    // create method is deprecated
+    const observable = new Observable((observer: Subscriber<any>) => {
+      observer.next("Hello");
+      observer.next("World");
+    });
+    observable.subscribe(value => console.log(value));
 
-    this.domEventObservable.subscribe(click => console.log(click));
 
-    this.observablePromise.subscribe(value => console.log(value));
+    // create an observable from a DOM event
+    const domEventObservable = fromEvent(document, 'click');
+    domEventObservable.subscribe(click => console.log(click));
 
-    this.observableTimer
+
+    // create an observable from a promise
+    const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      res("Resolved!");
+    }, 4000);
+    });
+    // fromPromise method is now wrapped inside the from method
+    const observablePromise = from(promise);
+    observablePromise.subscribe(value => console.log(value));
+
+
+    // create an observable timer
+    const observableTimer = timer(5000);
+    observableTimer
       // finalize executes when observable completes
       .pipe(finalize(() => console.log("Observable is complete.")))
       .subscribe(() => console.log("Timer finished!"));
 
+
+    // create observable time interval (emits something every interval)
+    const observableInterval = interval(1000);
     // store subscription in a variable to allow unsubscription
-    const intervalSubscription: Subscription = this.observableInterval.subscribe(i => console.log(i));
+    const intervalSubscription: Subscription = observableInterval.subscribe(i => console.log(i));
 
-    this.staticValueObservable.subscribe(value => console.log(value));
 
+    // obsrvable of static values
+    const staticValueObservable = of("anything", ["you", "want"], 23, true, {cool: "stuff"});
+    staticValueObservable.subscribe(value => console.log(value));
+
+    // unsubscrib to an observable
     setTimeout(() => {
       intervalSubscription.unsubscribe();
     }, 10000);
 
+    /* Cold vs Hot observables */
+    // Cold observable example
+    const cold = new Observable(observer => {
+      observer.next(Math.random());
+    });
     /* these result in two different values because observable does not
       generate a random number until subscribed to it (producer inside the observable)*/
     console.log("Cold Observable");
-    this.cold.subscribe(value => console.log(`Subscriber A: ${value}`));
-    this.cold.subscribe(value => console.log(`Subscriber B: ${value}`));
+    cold.subscribe(value => console.log(`Subscriber A: ${value}`));
+    cold.subscribe(value => console.log(`Subscriber B: ${value}`));
 
+
+    // Hot observable example
+    let randomNum = Math.random();
+    const hot = new Observable(observer => {
+      observer.next(randomNum);
+    })
     /* these output the same value because the producer is not contained inside the observable */
     console.log("Hot Observable");
-    this.hot.subscribe(value => console.log(`Subscriber A: ${value}`));
-    this.hot.subscribe(value => console.log(`Subscriber B: ${value}`));
+    hot.subscribe(value => console.log(`Subscriber A: ${value}`));
+    hot.subscribe(value => console.log(`Subscriber B: ${value}`));
 
-    /* Convert a cold observable to hot observable by calling publish
-      publish will allow subscribers to share the same values */
+    /* Convert a cold observable to hot observable by calling connectable
+      connectable will allow subscribers to share the same values */
     console.log("Cold observable converted to Hot");
-    const hotFromCold = connectable(this.cold); // publish operator is the same but it's depricated
+    const hotFromCold = connectable(cold); // publish operator is the same but it's depricated
     hotFromCold.subscribe(value => console.log(`Subscriber A: ${value}`));
     hotFromCold.subscribe(value => console.log(`Subscriber B: ${value}`));
     hotFromCold.connect(); // there won't be an output if connect() is not called
+
+
+    // Operators
+    // Map (A common use is to convert JSON response from an API call to a javascript object)
+    const numbers = from([10, 100, 1000]);
+    numbers
+      .pipe(map(num => Math.log(num)))
+      .subscribe(value => console.log(value));
 
   }
 
